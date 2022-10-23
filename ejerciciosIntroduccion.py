d4b9842b8a85e4e1c23521c8cda9d23c408d0f5d
hdevmei <hdevmei@gmail.com>
2022-10-23 20:07:19 +0200

    hechos ejercicios del 5 al 7

--- ejerciciosIntroduccion.py
+++ ejerciciosIntroduccion.py
@@ -30,7 +30,7 @@ def Ejercicio3():
         numRep = int(input("Numero de elementos: "))
         numList = []
         for x in range(numRep):
-            num = int(input("Numero para introducir: "))
+            num = int(input("Numero para poder introducir: "))
             numList.append(num)
         #numList = [0,1,2,7,"a",-5]
         numList.sort()
@@ -54,3 +54,104 @@ def Ejercicio4():
     except:
         print("El comando escrito no es un número")
 #Ejercicio4()
+#EJ 5
+def Ejercicio5():
+    plantDic = {"name":{},"type":{},"place":{}, "days":{},"size":{}}
+    while True:
+        global listadoPlantas
+        name = input("Nombre de la planta: ")
+        if len(listadoPlantas) <= 0:
+            plantDic["name"] = name
+            break
+        else:
+            for x in listadoPlantas:
+                if name == x["name"]:
+                    print("ERROR")
+                else:
+                    plantDic["name"] = name
+            if name != None:
+                break
+    plantDic["type"]=input("Tipo de la planta: ")
+    place = input("Lugar donde está la planta: ")
+    plantDic["place"] = place
+    wateringDays = input("Cada cuanto se riega la planta: ")
+    if wateringDays.isnumeric() and int(wateringDays) > 0:
+        plantDic["days"] = wateringDays
+    else:
+        print("El valor introducido no es un número entero positivo")
+    size = input("Introduce las medidas de la planta (en cm)")
+    if size.isnumeric() and int(size) > 0:
+        plantDic["size"] = str(size)+" cm"
+    else:
+        print("El valor introducido no es un número entero positivo")
+    print(plantDic)
+    return plantDic
+#Ejercicio5()
+
+#EJ 6
+def Ejercicio6():
+    global    listadoPlantas
+    def crearPlanta():
+        listadoPlantas.append(Ejercicio5())
+        print()
+    for x in range(2):
+        crearPlanta()
+    def mostrarInformacion(mostrar):
+        if mostrar:
+            for y in listadoPlantas:
+                print(y["name"])
+        else:
+            mostrarPlantas()
+            print("False")
+    def mostrarPlantas():
+        while True:
+            userInput = input("Quieres ver el nombre: y/n      ")
+            if userInput == "y":
+                mostrarInformacion(True)
+                break
+            elif userInput == "n":
+                while True:
+                    userInput = input("¿Quieres ver las plantas con los atributos?: y/n")
+                    if userInput == "y":
+                        mostrarInformacion(False)
+                        break
+                    elif userInput == "n":
+                        break
+                    else:
+                        print("No se reconoce el comando")
+                break
+            else:
+                print("No se reconoce el comando")
+
+    def modificarPlanta():
+        mostrarPlantas()
+        while True:
+            namePlant = input("Nombre de planta para cambiar: ")
+            for n in listadoPlantas:
+                if n["name"] == namePlant:
+                    atriChange = input("Atributo a cambiar: ")
+                    valorAtri = input("Valor nuevo: ")
+                    n[atriChange] = valorAtri
+                    return
+            print("No se encuentra la planta")
+    modificarPlanta()
+    print(listadoPlantas)
+    print("EJERCICIO 6")
+Ejercicio6()
+myTupla = ("UNO","DOS","TRES")
+#EJ 7
+def modTupla():
+    global myTupla
+    myTupla = list(myTupla)
+    myTupla[1] = "CUATRO"
+    myTupla = tuple(myTupla)
+    return myTupla
+
+
+def Ejercicio7():
+    #myTupla = ("UNO","DOS","TRES")
+    global myTupla
+    modTupla()
+    print(myTupla)
+
+#Ejercicio7()
